9cb53f301d87578db417d6841d7ca5e9aac57e3e
Daniil Kutyavin <daniil.kutyavin94@gmail.com>
2020-07-03 00:04:15 +0300

    update basics lists

--- week-1/basics-lists.rkt
+++ week-1/basics-lists.rkt
@@ -2,7 +2,22 @@
 
 (provide (all-defined-out))
 
+; sum
 (define (sum xs)
   (if (null? xs)
     0
     (+ (car xs) (sum (cdr xs)))))
+
+; append
+(define (my-append xs ys)
+  (if (null? xs)
+      ys
+      (cons (car xs) (my-append (cdr xs) ys))))
+
+; map
+(define (my-map f xs)
+  (if (null? xs)
+      null
+      (cons (f (car xs))
+            (my-map f (cdr xs)))))
+  
